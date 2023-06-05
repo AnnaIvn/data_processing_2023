c411c2e5b38f68b2ca427f33aa5ac25837778832
Ivanytska Anna <aniaiv12345@gmail.com>
2023-06-05 14:51:14 +0300

    third commit of lab 5 a little cleaning

--- src/main/java/ua/knu/fit/mit/lab5/Lab5Application.java
+++ src/main/java/ua/knu/fit/mit/lab5/Lab5Application.java
@@ -1,18 +1,9 @@
 package ua.knu.fit.mit.lab5;
-
-import components.Sample;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
 
 @SpringBootApplication
 public class Lab5Application {
-
-	@Autowired
-	Sample sample;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Lab5Application.class, args);
